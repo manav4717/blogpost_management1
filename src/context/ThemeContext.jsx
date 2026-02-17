49ff00c731829249120a84fb955a12a79bbbb6d7
Manav Goswami <manavbharthigoswami29@gmail.com>
2026-02-17 13:37:20 +0530

    [MG][all file]

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Children } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
-function ThemeContext() {
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ Children }) => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme || "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <>
-      
-    </>
-  )
-}
-
-export default ThemeContext
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {Children}
+    </ThemeContext.Provider>
+  );
+};
